4049403ae5f778b7cabdb9c94edcba073254de14
GalvinGao <me@galvingao.com>
2023-04-21 23:08:08 -0400

    feat: savepoint

--- src/components/Pulsars.tsx
+++ src/components/Pulsars.tsx
@@ -1,92 +1,62 @@
 import { shaderMaterial } from '@react-three/drei'
 import { extend } from '@react-three/fiber'
 import { TransformedPulsar } from 'models/pulsars'
-import niceColors from 'nice-color-palettes'
-import { useLayoutEffect, useMemo, useRef } from 'react'
+import { useLayoutEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { InstancedMesh } from 'three'
 
 const o = new THREE.Object3D()
-const c = new THREE.Color()
 
-const size = [0.1, 0.1, 0.1] as [number, number, number]
+const scaleFactor = 1e2
 
-const MeshEdgesMaterial = shaderMaterial(
-	{
-		color: new THREE.Color('white'),
-		size: new THREE.Vector3(1, 1, 1),
-		thickness: 0.01,
-		smoothness: 0.2
-	},
-	/*glsl*/ `varying vec3 vPosition;
+const MeshGlowingMaterial = shaderMaterial(
+	{ glowFalloff: 1 },
+	`varying vec3 vNormal;
   void main() {
-    vPosition = position;
+    vNormal = normalize(normalMatrix * normal);
     gl_Position = projectionMatrix * viewMatrix * instanceMatrix * vec4(position, 1.0);
   }`,
-	/*glsl*/ `varying vec3 vPosition;
-  uniform vec3 size;
-  uniform vec3 color;
-  uniform float thickness;
-  uniform float smoothness;
+	`varying vec3 vNormal;
+  uniform float glowFalloff;
   void main() {
-    vec3 d = abs(vPosition) - (size * 0.5);
-    float a = smoothstep(thickness, thickness + smoothness, min(min(length(d.xy), length(d.yz)), length(d.xz)));
-    gl_FragColor = vec4(color, 1.0 - a);
+    vec3 light = vec3(0, 0.5, 0);
+    float intensity = pow(dot(vNormal, light), glowFalloff);
+    vec3 baseColor = vec3(1.0, 1.0, 1.0);
+    vec3 glowColor = vec3(1.0, 1.0, 1.0);
+    vec3 finalColor = mix(baseColor, glowColor, intensity);
+    gl_FragColor = vec4(finalColor, 1.0);
   }`
 )
-extend({ MeshEdgesMaterial })
+
+extend({ MeshGlowingMaterial })
 
 export function Pulsars({ pulsars }: { pulsars: TransformedPulsar[] }) {
 	const reference = useRef<InstancedMesh | null | undefined>(undefined)
-	const outlines = useRef<InstancedMesh | null | undefined>(undefined)
-	const colors = useMemo(
-		() =>
-			new Float32Array(
-				Array.from({ length: pulsars.length }, () =>
-					c.set(niceColors[17][Math.floor(Math.random() * 5)]).toArray()
-				).flat()
-			),
-		[pulsars.length]
-	)
 	useLayoutEffect(() => {
-		if (!reference.current || !outlines.current) return
+		if (!reference.current) return
 
 		let index = 0
 		for (const pulsar of pulsars) {
 			// o.rotation.set(Math.random(), Math.random(), Math.random())
 			if (!pulsar.position) return
 			const id = index++
-			o.position.set(pulsar.position.x, pulsar.position.y, pulsar.position.z)
+			o.position.set(
+				pulsar.position.x * scaleFactor,
+				pulsar.position.y * scaleFactor,
+				pulsar.position.z * scaleFactor
+			)
 			o.updateMatrix()
 			reference.current.setMatrixAt(id, o.matrix)
 		}
+		console.log('index', index)
 		reference.current.instanceMatrix.needsUpdate = true
-		// Re-use geometry + instance matrix
-		outlines.current.geometry = reference.current.geometry
-		outlines.current.instanceMatrix = reference.current.instanceMatrix
 	}, [pulsars])
 
 	return (
 		<group>
 			<instancedMesh ref={reference} args={[null, null, pulsars.length]}>
-				<boxGeometry args={size}>
-					<instancedBufferAttribute
-						attach='attributes-color'
-						args={[colors, 3]}
-					/>
-				</boxGeometry>
-				<meshLambertMaterial vertexColors toneMapped={false} />
-			</instancedMesh>
-			<instancedMesh ref={outlines} args={[null, null, pulsars.length]}>
-				<meshEdgesMaterial
-					transparent
-					polygonOffset
-					polygonOffsetFactor={-10}
-					size={size}
-					color='black'
-					thickness={0.001}
-					smoothness={0.005}
-				/>
+				<sphereGeometry args={[0.2]} />
+				<meshGlowingMaterial />
 			</instancedMesh>
 		</group>
 	)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { CameraControls } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
 import { DevelopmentUtils } from 'components/DevelopmentUtils'
@@ -11,7 +11,9 @@ function PulsarLoader(): ReactElement {
 	const [pulsars, setPulsars] = useState<TransformedPulsar[]>([])
 
 	useLayoutEffect(() => {
-		getPulsars().then(setPulsars)
+		getPulsars()
+			.then(gotPulsars => gotPulsars.filter(pulsar => pulsar.position))
+			.then(setPulsars)
 	}, [])
 
 	if (pulsars.length === 0) {
@@ -22,10 +24,18 @@ function PulsarLoader(): ReactElement {
 }
 
 export default function HomePage(): ReactElement {
+	// const cameraControlRef = useRef<CameraControls | null>(null)
+
+	// useFrame(() => {
+	// 	if (!cameraControlRef.current) return
+
+	// 	cameraControlRef.current.
+	// })
+
 	return (
 		<>
 			<Head title='Map' />
-			<Canvas>
+			<Canvas camera={{ position: [0, 0, 10], far: 10_000 }}>
 				<ambientLight intensity={0.85} />
 				<directionalLight position={[150, 150, 150]} intensity={1} />
 				{/* <mesh>
@@ -35,13 +45,10 @@ export default function HomePage(): ReactElement {
 				<PulsarLoader />
 				<mesh>
 					<sphereGeometry args={[0.1]} />
-					<meshStandardMaterial
-						color='hotpink'
-						emissive='blue'
-						emissiveIntensity={0.2}
-					/>
+					<meshStandardMaterial color='red' emissiveIntensity={0.2} />
 				</mesh>
-				<CameraControls makeDefault />
+				<PerspectiveCamera makeDefault position={[0, 0, 10]} />
+				<OrbitControls minDistance={3} maxDistance={1e3} />
 				<EffectComposer>
 					<Bloom
 						luminanceThreshold={0.1}
